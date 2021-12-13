6ef2f4003b7ff2b9649a4f7139f4ea8c8b3872b0
Rami Mohsen <Rami.Mohsen@integrant.com>
2021-12-13 18:16:21 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using MongoDB.Driver;
+
+namespace MongoDBConnectionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            MongoClient dbClient = new MongoClient("mongodb+srv://sa:admin123@cluster0.lftks.mongodb.net/Cluster0?retryWrites=true&w=majority");
+
+            var dbList = dbClient.ListDatabases().ToList();
+
+            Console.WriteLine("The list of databases on this server is: ");
+            foreach (var db in dbList)
+            {
+                Console.WriteLine(db);
+            }
+        }
+
+    }
+}
